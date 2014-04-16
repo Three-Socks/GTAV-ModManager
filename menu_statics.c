67dbe2e986aa296ff0e4319f44e719bbea195f9a
ThreeSocks <mark@playstationgl.com>
2014-04-16 21:41:37 +0100

    Update.

---
+++ menu_statics.c
@@ -0,0 +1,105 @@
+/*
+	Statics 1-327 Menu reserved
+*/
+
+// Not used.
+Static_2 = 1;
+
+// (float) menu_x
+Static_3 = 0.0453125f;
+
+// (uint) menu_item_highlighted
+Static_4 = 0;
+
+// (uint) menu_count
+Static_5 = 0;
+
+// (bool) menu_hidden
+Static_6 = 0;
+
+// (uint) menu_action_mode (1 = forward, 2= back)
+Static_7 = 0;
+
+// (uint) menu_load_hold_pressed
+Static_8 = 0;
+
+// (uint) menu_level
+Static_9 = 0;
+
+// (char) menu_header
+Static_10 = 0;
+
+// (bool) menu_items_set
+Static_11 = 0;
+
+// (int) instructional_buttons scaleform movie
+Static_12 = 0;
+
+// (bool) scaleform_movie requested
+Static_13 = 0;
+
+// Counters
+//(bool) reset_counter
+Static_14 = 0;
+
+//(uint) press_counter
+Static_15 = 2;
+
+// (uint) hold_counter
+Static_16 = 0;
+
+// (uint) press_counter_timesby
+Static_17 = 1;
+
+// (uint) press_id
+Static_18 = 0;
+
+// (uint) menu_unload_hold_pressed
+Static_19 = 0;
+
+// (float) menu_start_y
+Static_20
+
+// (float) menu_consts_start_y
+Static_21
+
+// (float) menu_spacing
+Static_22
+
+// (uint) menu_max
+Static_23
+
+// (uint) menu_consts_max
+Static_24
+
+// (uint) menu_start_scrolling
+Static_25
+
+// (uint) menu_sub_action_mode
+Static_26
+
+// (uint) last_selected (10)
+Static[30]
+
+// (char) menu_items_name (50)
+Static[72]
+
+// (uint) menu_items_type (1 = number, 2 = float, 3 = bool, 4 = GXT, 5 = vehicle hash) (50)
+Static[123]
+
+// (int) menu_items_extra_val (50)
+Static[174]
+
+// (bool) menu_items_action (50)
+Static[225]
+
+// (int) menu_items_num_val (50)
+Static[276]
+
+// (float) menu_items_float_val (50)
+Static[327]
+
+// ModManager
+
+// (float) menu_items_float_val
+Static[327]
