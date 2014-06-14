4a24b2e07950edefca14a46eeb4a82a790c40a65
ThreeSocks <mark@playstationgl.com>
2014-06-14 09:03:01 +0100

    Updated menu_statics.c.

--- menu_statics.c
+++ menu_statics.c
@@ -91,33 +91,33 @@ Static_45
 Static_46
 
 // (char) frontend_items_button (6)
-Static[47]
+Static_47[* 6]
 
 // (char) frontend_items_name (6)
-Static[54]
+Static_54[* 6]
 
 // (uint) last_selected (10)
-Static[61]
+Static_61[* 10]
 
 // (char) menu_items_name (50)
-Static[72]
+Static_72[* 50]
 
-// (uint) menu_items_type (1 = number, 2 = float, 3 = bool, 4 = GXT, 5 = vehicle hash, 6 = keyboard string, 7 = keyboard number, 8 = add number, 9 = gxt add number) (50)
-Static[123]
+// (uint) menu_items_type (1 = number, 2 = float, 3 = bool, 4 = GXT, 5 = vehicle hash, 6 = keyboard string, 7 = keyboard number, 8 = add number, 9 = gxt add number, 10 = gxt add number2, 11 = gxt number) (50)
+Static_123[* 50]
 
 // (int) menu_items_extra_val (50)
-Static[174]
+Static_174[* 50]
 
 // (bool) menu_items_action (50)
-Static[225]
+Static_225[* 50]
 
 // (int) menu_items_num_val (50)
-Static[276]
+Static_276[* 50]
 
 // (float) menu_items_float_val (50)
-Static[327]
+Static_327[* 50]
 
 // ModManager
 
 // (bool) modmanager_item_menu (50)
-Static[378]
+Static_378[* 50]
